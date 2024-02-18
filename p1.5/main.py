764f67d481865c3b90f61ef3b6c79ea5460cbc23
Flower2103 <al22760045@ite.edu.mx>
2024-02-18 09:52:55 -0800

    add: Se elimina redundancia de código y se edita documentación.

--- p1.5/main.py
+++ p1.5/main.py
@@ -1,7 +1,10 @@
 import json
 
-# Diccionario para almacenar los tokens
+# Diccionario para almacenar los tokens del archivo .txt
 tokens = {}
+
+# Lista para almacenar el resultado de tokens y caracteres encontrados en el archivo .json
+resultado_tokens = []
 
 # Cargar tokens desde el archivo
 with open('p1.5/catalogo_tokens.txt', 'r') as archivo_tokens:
@@ -9,14 +12,17 @@ with open('p1.5/catalogo_tokens.txt', 'r') as archivo_tokens:
         clave, valor = linea.strip().split(':')
         tokens[clave] = valor
 
-# Lista para almacenar los resultados
-resultado_tokens = []
 
 # Abrir archivo JSON, leer y asignarlo a la variable data
 with open('p1.5/nombre.json', 'r') as myfile:
     data = myfile.read()
 
-# Recorrer los caracteres del archivo JSON y generar los tokens correspondientes
+
+"""
+# Recorre los caracteres de la variable "data" y almacena los tokens y caracteres
+  correspondientes en  <list> "resultado_tokens" si estos se encuentra en el <dic> "tokens", 
+  los que no se encuentran en el diccionrio se condicionaron.
+"""
 for caracter in data:
     if caracter in tokens:
         resultado_tokens.append((tokens[caracter], caracter))
@@ -27,13 +33,6 @@ for caracter in data:
     elif caracter == ':':
         resultado_tokens.append(('58', ':'))         
 
-# Imprimir los resultados
+# Imprime los resultados de la lista
 for token, caracter in resultado_tokens:
-    if token == '32':
-        print(f"{token} := {caracter}")
-    elif token == '10':
-        print(f"{token} := {caracter}")
-    elif token == '58':
-        print(f"{token} := {caracter}")
-    else:
-        print(f"{token} := {caracter}")
+    print(f"{token} := {caracter}")

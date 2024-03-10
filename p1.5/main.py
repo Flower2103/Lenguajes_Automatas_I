68b67c296491dfe19dac339cb8b1589e594e86d5
Flower2103 <al22760045@ite.edu.mx>
2024-03-10 00:16:02 -0800

    Se implementan funciones para optimizar el código

--- p1.5/main.py
+++ p1.5/main.py
@@ -1,21 +1,20 @@
 import json
 
-# Diccionario para almacenar los tokens del archivo .txt
-tokens = {}
+tokens = {} # Diccionario para almacenar los tokens del archivo .txt
 
-# Lista para almacenar el resultado de tokens y caracteres encontrados en el archivo .json
-resultado_tokens = []
-
-# Cargar tokens desde el archivo
-with open('p1.5/catalogo_tokens.txt', 'r') as archivo_tokens:
-    for linea in archivo_tokens:
-        clave, valor = linea.strip().split(':')
-        tokens[clave] = valor
+# Cargar tokens desde el archivo.txt
+def catalogo_tokens():
+    with open('p1.5/catalogo_tokens.txt', 'r') as archivo_tokens:
+        for linea in archivo_tokens:
+            clave, valor = linea.strip().split(':')
+            tokens[clave] = valor
 
 
 # Abrir archivo JSON, leer y asignarlo a la variable data
-with open('p1.5/nombre.json', 'r') as myfile:
-    data = myfile.read()
+def leer_json():
+    with open('p1.5/nombre.json', 'r') as myfile:
+        data = myfile.read()
+    return data
 
 
 """
@@ -23,16 +22,28 @@ with open('p1.5/nombre.json', 'r') as myfile:
   correspondientes en  <list> "resultado_tokens" si estos se encuentra en el <dic> "tokens", 
   los que no se encuentran en el diccionrio se condicionaron.
 """
-for caracter in data:
-    if caracter in tokens:
-        resultado_tokens.append((tokens[caracter], caracter))
-    elif caracter == ' ':
-        resultado_tokens.append(('32', '[ESPACIO]'))  
-    elif caracter == '\n':
-        resultado_tokens.append(('10', '[ENTER]'))    
-    elif caracter == ':':
-        resultado_tokens.append(('58', ':'))         
+def procesar_caracteres(data, tokens):
+    resultado_tokens = [] # Lista para almacenar el resultado
+    for caracter in data:
+        if caracter in tokens:
+            resultado_tokens.append((tokens[caracter], caracter))
+        elif caracter == ' ':
+            resultado_tokens.append(('32', '[ESPACIO]'))  
+        elif caracter == '\n':
+            resultado_tokens.append(('10', '[ENTER]'))    
+        elif caracter == ':':
+            resultado_tokens.append(('58', ':')) 
+    return resultado_tokens        
 
-# Imprime los resultados de la lista
-for token, caracter in resultado_tokens:
-    print(f"{token} := {caracter}")
+
+def main():
+    catalogo_tokens()
+    data = leer_json()
+    resultado = procesar_caracteres(data , tokens)
+    # Imprime los resultados de la lista
+    for token, caracter in resultado:
+        print(f"{token} := {caracter}")
+
+
+if __name__=="__main__":
+    main()
